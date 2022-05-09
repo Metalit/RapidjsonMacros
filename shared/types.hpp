5b363a1919fdce2484b1ce16e385dc5efb9f5bcb
Metalit <kylesmithhanna@gmail.com>
2022-05-08 20:54:23 -0500

    Multifile bugfix

--- shared/types.hpp
+++ shared/types.hpp
@@ -33,68 +33,68 @@ concept JSONClassDerived = std::is_base_of_v<JSONClass, T>;
 namespace rapidjson_macros_types {
 
     template<class T, class R, std::size_t N = 0>
-    R GetJSONString(const T& string, rapidjson::Document::AllocatorType& allocator);
+    inline R GetJSONString(const T& string, rapidjson::Document::AllocatorType& allocator);
 
     template<class T, std::size_t N = 0>
     requires(std::is_convertible_v<T, std::string>)
-    rapidjson::Value GetJSONString(const T& string, rapidjson::Document::AllocatorType& allocator) {
+    inline rapidjson::Value GetJSONString(const T& string, rapidjson::Document::AllocatorType& allocator) {
         return rapidjson::Value(string, allocator);
     }
     template<std::size_t N = 0>
-    rapidjson::Value::StringRefType GetJSONString(const char (&string)[N], rapidjson::Document::AllocatorType& allocator) {
+    inline rapidjson::Value::StringRefType GetJSONString(const char (&string)[N], rapidjson::Document::AllocatorType& allocator) {
         return rapidjson::Value::StringRefType(string);
     }
 
     template<class T>
-    rapidjson::Value CreateJSONValue(T& value, rapidjson::Document::AllocatorType& allocator) {
+    inline rapidjson::Value CreateJSONValue(T& value, rapidjson::Document::AllocatorType& allocator) {
         return rapidjson::Value(value);
     }
     template<>
-    rapidjson::Value CreateJSONValue(const std::string& value, rapidjson::Document::AllocatorType& allocator) {
+    inline rapidjson::Value CreateJSONValue(const std::string& value, rapidjson::Document::AllocatorType& allocator) {
         return rapidjson::Value(value, allocator);
     }
     template<>
-    rapidjson::Value CreateJSONValue(std::string& value, rapidjson::Document::AllocatorType& allocator) {
+    inline rapidjson::Value CreateJSONValue(std::string& value, rapidjson::Document::AllocatorType& allocator) {
         return rapidjson::Value(value, allocator);
     }
     
     template<class T>
-    T GetValueType(const rapidjson::Value& jsonValue, const T& _) {
+    inline T GetValueType(const rapidjson::Value& jsonValue, const T& _) {
         return jsonValue.Get<T>();
     }
 
     template<class T>
-    bool GetIsType(const rapidjson::Value& jsonValue, const T& _) {
+    inline bool GetIsType(const rapidjson::Value& jsonValue, const T& _) {
         return jsonValue.Is<T>();
     }
 
     template<class T>
-    T GetValueTypeOptional(const rapidjson::Value& jsonValue, const std::optional<T>& _) {
+    inline T GetValueTypeOptional(const rapidjson::Value& jsonValue, const std::optional<T>& _) {
         return jsonValue.Get<T>();
     }
 
     template<class T>
-    bool GetIsTypeOptional(const rapidjson::Value& jsonValue, const std::optional<T>& _) {
+    inline bool GetIsTypeOptional(const rapidjson::Value& jsonValue, const std::optional<T>& _) {
         return jsonValue.Is<T>();
     }
 
     template<class T>
-    T GetValueTypeVector(const rapidjson::Value& jsonValue, const std::vector<T>& _) {
+    inline T GetValueTypeVector(const rapidjson::Value& jsonValue, const std::vector<T>& _) {
         return jsonValue.Get<T>();
     }
 
     template<class T>
-    T GetValueTypeVectorOptional(const rapidjson::Value& jsonValue, const std::optional<std::vector<T>>& _) {
+    inline T GetValueTypeVectorOptional(const rapidjson::Value& jsonValue, const std::optional<std::vector<T>>& _) {
         return jsonValue.Get<T>();
     }
 
     template<class T>
-    T NewVectorType(const std::vector<T>& _) {
+    inline T NewVectorType(const std::vector<T>& _) {
         return T();
     }
 
     template<class T>
-    T NewVectorTypeOptional(const std::optional<std::vector<T>>& _) {
+    inline T NewVectorTypeOptional(const std::optional<std::vector<T>>& _) {
         return T();
     }
 }

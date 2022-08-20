f8959f060fbe5592bddc3372cea670fb0737feb7
Metalit <48568911+Metalit@users.noreply.github.com>
2022-08-20 16:56:41 -0500

    Add regions for convenience

--- shared/macros.hpp
+++ shared/macros.hpp
@@ -3,6 +3,7 @@
 #include "../shared/auto.hpp"
 
 // declare a class with serialization and deserialization support using ReadFromFile and WriteToFile
+#pragma region DECLARE_JSON_CLASS(namespace, name, fields)
 #define DECLARE_JSON_CLASS(namespaze, name, ...) \
 namespace namespaze { \
     class name : public JSONClass { \
@@ -31,8 +32,10 @@ namespace namespaze { \
             __VA_ARGS__ \
     }; \
 }
+#pragma endregion
 
 // prevents the class from preserving json data not specified in class fields and serialization
+#pragma region DISCARD_EXTRA_FIELDS
 #define DISCARD_EXTRA_FIELDS \
 private: \
 friend struct _ExtraFieldBoolSetter; \
@@ -42,9 +45,11 @@ struct _ExtraFieldBoolSetter { \
     } \
 }; \
 static inline _ExtraFieldBoolSetter _ExtraFieldBoolSetterInstance;
+#pragma endregion
 
 // add an action to be run during deserialization (requires an identifier unique to the class)
 // will most likely be run in the order of fields in your class definition
+#pragma region DESERIALIZE_ACTION(id, body)
 #define DESERIALIZE_ACTION(uid, ...) \
 private: \
 struct _DeserializeAction_##uid { \
@@ -55,9 +60,11 @@ struct _DeserializeAction_##uid { \
     } \
 }; \
 static inline _DeserializeAction_##uid _##uid##_DeserializeActionInstance;
+#pragma endregion
 
 // add an action to be run during serialization (requires an identifier unique to the class)
 // will most likely be run in the order of fields in your class definition
+#pragma region SERIALIZE_ACTION(id, body)
 #define SERIALIZE_ACTION(uid, ...) \
 private: \
 struct _SerializeAction_##uid { \
@@ -68,8 +75,10 @@ struct _SerializeAction_##uid { \
     } \
 }; \
 static inline _SerializeAction_##uid _##uid##_SerializeActionInstance;
+#pragma endregion
 
 // define an automatically serialized / deserialized instance variable with a custom name in the json file
+#pragma region NAMED_VALUE(type, name, jsonName)
 #define NAMED_VALUE(type, name, jsonName) \
 public: \
 type name; \
@@ -85,8 +94,10 @@ struct _JSONValueAdder_##name { \
     } \
 }; \
 static inline _JSONValueAdder_##name _##name##_JSONValueAdderInstance;
+#pragma endregion
 
 // define an automatically serialized / deserialized std::optional instance variable with a custom name in the json file
+#pragma region NAMED_VALUE_OPTIONAL(type, name, jsonName)
 #define NAMED_VALUE_OPTIONAL(type, name, jsonName) \
 public: \
 std::optional<type> name = std::nullopt; \
@@ -102,8 +113,10 @@ struct _JSONValueAdder_##name { \
     } \
 }; \
 static inline _JSONValueAdder_##name _##name##_JSONValueAdderInstance;
+#pragma endregion
 
 // define an automatically serialized / deserialized instance variable with a custom name in the json file and a default value
+#pragma region NAMED_VALUE_DEFAULT(type, name, default, jsonName)
 #define NAMED_VALUE_DEFAULT(type, name, def, jsonName) \
 public: \
 type name = def; \
@@ -119,6 +132,7 @@ struct _JSONValueAdder_##name { \
     } \
 }; \
 static inline _JSONValueAdder_##name _##name##_JSONValueAdderInstance;
+#pragma endregion
 
 // define an automatically serialized / deserialized std::vector with a custom name in the json file
 #define NAMED_VECTOR(type, name, jsonName) NAMED_VALUE(std::vector<type>, name, jsonName)

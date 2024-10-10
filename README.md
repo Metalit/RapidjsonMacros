55a95765d53c6b5705db8ffc0cf765ba996b82bd
Metalit <48568911+Metalit@users.noreply.github.com>
2024-10-10 11:32:56 -0600

    Better build script

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # RapidjsonMacros
 
-A set of useful macros to make working with complicated and nested rapidjson types a little easier.
+A library of macros to create c++ structs that can validate and parse almost any json structure.
+
+## How To Use
+
+This is a QPM package, so run `qpm dependency add rapidjson-macros` and `qpm restore`. It's also used in the `config-utils` package.
+
+Documentation can be found in the comments in `macros.hpp`.
+
+## Local Test
+
+Make sure gcc is on path and VCPKG_ROOT is set. Run `vcpkg install rapidjson` and optionally update the path in `build.ps1` if needed. Then run `build.ps1` and `test.ps1`.
--- build.ps1
+++ build.ps1
@@ -1,5 +1,13 @@
+$triplet = "x64-windows"
 
-$rapidjson="C:/Path/vcpkg/packages/rapidjson_x64-mingw-dynamic/include"
-$rapidjson_macros="shared/rapidjson-macros"
+if ($null -eq $env:VCPKG_ROOT -or (Test-Path $env:VCPKG_ROOT) -eq $false) {
+    Write-Error "VCPKG_ROOT not found"
+}
 
-g++ -std=c++20 -DRAPIDJSON_MACROS_GCC_TEST -I"$rapidjson" -I"$rapidjson_macros" -Iinclude -Ishared ./src/*.cpp -o rapidjsontest.exe
+$rapidjson = Join-Path $env:VCPKG_ROOT "installed/$triplet/include/rapidjson"
+
+if ((Test-Path $rapidjson) -eq $false) {
+    Write-Error "rapidjson not found"
+}
+
+g++ -std=c++20 -DRAPIDJSON_MACROS_GCC_TEST -I"$rapidjson" -Iinclude -Ishared ./src/*.cpp -o rapidjsontest.exe
